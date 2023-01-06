b0af2c1e2b68820c934fee726c027c0d1e3699b4
Mathieu Segaud <segaud.mathieu@gmail.com>
2023-01-06 08:05:26 +0100

    responsive improved

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,21 +3,19 @@ import Collapse from "../components/Collapse";
 import ImgAbout from "../assets/img-about.png";
 import HomeImg from "../components/HomeImg";
 
-
 const About = ({ dataAbout }) => {
-
 	return (
 		<div>
 			<HomeImg backgroundImg={ImgAbout} />
 			<main>
 				<article className="collapse-container --about">
 					{dataAbout.map(({ title, description, id }) => (
-					<Collapse
-						title={title}
-						collapseWidth={"80%"}
-						description={description}
-						key={title + id}
-					/>
+						<Collapse
+							title={title}
+							collapseWidth={"80%"}
+							description={description}
+							key={title + id}
+						/>
 					))}
 				</article>
 			</main>
